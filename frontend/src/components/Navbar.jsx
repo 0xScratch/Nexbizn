456209b915cbc7cb983ba5b64a727e3ae119ea36
Aryan Malik <aryanhunny98@gmail.com>
2024-03-31 13:15:30 +0530

    Added Screenshots

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -69,6 +69,11 @@ const Navbar = () => {
                                     <p>Auction</p>
                                 </Tab>
                             </Link>
+                            <Link href="https://github.com/0xScratch/Nexbizn/blob/main/README.md">
+                                <Tab>
+                                    <p>About</p>
+                                </Tab>
+                            </Link>
                         </TabList>
                         <TabIndicator
                             mt="-5px"
